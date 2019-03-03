44ce300438f3de7c17725c09c1056e9953d32d9e
dunyung1 <dshege4@gmail.com>
2019-03-03 07:57:59 +0300

    creating relationships

--- gallery/models.py
+++ gallery/models.py
@@ -4,6 +4,8 @@ class Image(models.Model):
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =50)
     image = models.ImageField(upload_to = 'photos/')
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.name
