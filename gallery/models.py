e88aa6c8d4978562eed3b6b3f06c0a1f7a04a633
dunyung1 <dshege4@gmail.com>
2019-03-03 08:28:50 +0300

    delete original migrations

--- gallery/models.py
+++ gallery/models.py
@@ -1,14 +1,5 @@
 from django.db import models
-
-class Image(models.Model):
-    name = models.CharField(max_length =30)
-    description = models.CharField(max_length =50)
-    image = models.ImageField(upload_to = 'photos/')
-    location = models.ForeignKey(Location)
-    category = models.ManyToManyField(Category)
-
-    def __str__(self):
-        return self.name
+import datetime as dt 
 
 class Location(models.Model):
     lname = models.CharField(max_length =30)
@@ -21,6 +12,16 @@ class Category(models.Model):
 
     def __str__(self):
         return self.cname
+
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.CharField(max_length =50)
+    image = models.ImageField(upload_to = 'photos/')
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
 
 
 
