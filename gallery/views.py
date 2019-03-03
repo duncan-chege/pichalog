4b12d22fa785b087e80d72f295c0200418c8ba50
dunyung1 <dshege4@gmail.com>
2019-03-03 22:17:23 +0300

    call search by title class method & render the html

--- gallery/views.py
+++ gallery/views.py
@@ -10,4 +10,6 @@ def display_images(request):
 def search_results(request):
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_images = 
+        searched_images = Image.search_by_category(search_term)
+        
+        return render(request, "search.html",{"images": searched_images})       #articles is a key while searched_images is a value
