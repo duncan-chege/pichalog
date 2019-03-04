eda64c13d31f62375a86a00a466257b4ac1c1dcf
dunyung1 <dshege4@gmail.com>
2019-03-04 19:07:22 +0300

    put specific save location an d save category functions

--- gallery/models.py
+++ gallery/models.py
@@ -8,6 +8,8 @@ class Location(models.Model):
     def __str__(self):
         return self.lname
 
+    def save_location(self):
+        self.save()
 
 class Category(models.Model):
     cname = models.CharField(max_length=30)
@@ -17,6 +19,9 @@ class Category(models.Model):
 
     class Meta:  # Meta subclass specifies model-specific options. This helps in ordering data
         verbose_name_plural = 'Categories'
+
+    def save_category(self):
+        self.save()
 
 
 class Image(models.Model):
