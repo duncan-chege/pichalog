663d9ad16aa3e0373bdfc3759962ae05a0591576
dunyung1 <dshege4@gmail.com>
2019-03-05 09:07:29 +0300

    remove commented out tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -28,30 +28,3 @@ class ImageTestClass(TestCase):
         self.new_image.save_image()
         get_specific_image = Location.objects.get(lname="Nairobi")
         self.assertTrue(get_specific_image.lname=="Nairobi")
-
-
-
-# class CategoryTestClass(TestCase):
-#     def setUp(self):
-#          self.test_cname=Category(cname="travel")
-
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.test_cname,Category))
-
-# class ImageTestClass(TestCase):
-#     def setUp(self):
-#         self.test_lname=Location(lname="Kisumu")
-#         self.test_lname.save()
-
-#         self.test_cname=Category(cname="travel")
-#         self.test_cname.save()
-
-#         self.test_image=Image(id=6, name="male.jpg",description="cool_guy",image="image",location=self.test_lname)
-#         self.test_image.save()
-#         # self.test_image.category.add(self.test_category)
-
-#     def test_save_the_image(self):
-#         self.test_image.save_image()
-#         images = Image.objects.all()
-#         self.assertTrue(len(images) > 0)
-
